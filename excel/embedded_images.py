4eda095b46d998fc7e5a60006c6e64549fc0cd76
unknown <mc.raul86@gmail.com>
2024-04-14 22:48:06 -0400

    Ejemplos excel con libreria xlsxwriter

---
+++ excel/embedded_images.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+# An example of embedding images into a worksheet cells using the XlsxWriter
+# Python module.
+#
+# This is the equivalent of Excel's menu option to insert an image using the
+# option to "Place in Cell" which is only available in Excel 365 versions from
+# 2023 onwards. For older versions of Excel a ``#VALUE!`` error is displayed.
+#
+# SPDX-License-Identifier: BSD-2-Clause Copyright 2013-2024, John McNamara,
+# jmcnamara@cpan.org
+#
+import xlsxwriter
+
+
+# Create an new Excel file and add a worksheet.
+workbook = xlsxwriter.Workbook("embedded_images.xlsx")
+worksheet = workbook.add_worksheet()
+
+# Widen the first column to make the caption clearer.
+worksheet.set_column(0, 0, 30)
+worksheet.write(0, 0, "Embed images that scale to cell size")
+
+# Embed an images in cells of different widths/heights.
+worksheet.set_column(1, 1, 14)
+
+worksheet.set_row(1, 60)
+worksheet.embed_image(1, 1, "python.png")
+
+worksheet.set_row(3, 120)
+worksheet.embed_image(3, 1, "python.png")
+
+workbook.close()
---
+++ excel/lambda.py
@@ -0,0 +1,34 @@
+#######################################################################
+#
+# An example of using the new Excel LAMBDA() function with the XlsxWriter
+# module.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright 2013-2024, John McNamara, jmcnamara@cpan.org
+#
+from xlsxwriter.workbook import Workbook
+
+workbook = Workbook("lambda.xlsx")
+worksheet = workbook.add_worksheet()
+
+
+# Write a Lambda function to convert Fahrenheit to Celsius to a cell.
+#
+# Note that the lambda function parameters must be prefixed with
+# "_xlpm.". These prefixes won't show up in Excel.
+worksheet.write("A1", "=LAMBDA(_xlpm.temp, (5/9) * (_xlpm.temp-32))(32)")
+
+# Create the same formula (without an argument) as a defined name and use that
+# to calculate a value.
+#
+# Note that the formula name is prefixed with "_xlfn." (this is normally
+# converted automatically by write_formula() but isn't for defined names)
+# and note that the lambda function parameters are prefixed with
+# "_xlpm.". These prefixes won't show up in Excel.
+workbook.define_name("ToCelsius", "=_xlfn.LAMBDA(_xlpm.temp, (5/9) * (_xlpm.temp-32))")
+
+# The user defined name needs to be written explicitly as a dynamic array
+# formula.
+worksheet.write_dynamic_array_formula("A2", "=ToCelsius(212)")
+
+workbook.close()
